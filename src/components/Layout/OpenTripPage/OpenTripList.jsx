6c77dfe8dd738642af49805792d6cb29eb046b18
Aqil Rahmatullah <aqilr54@gmail.com>
2024-05-25 00:03:54 +0800

    feat: pagination on open trip page

--- src/components/Layout/OpenTripPage/OpenTripList.jsx
+++ src/components/Layout/OpenTripPage/OpenTripList.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { OpenTripItem } from './OpenTripItem';
 
 function OpenTripList() {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
 
   useEffect(() => {
     const tripData = [
@@ -34,27 +35,144 @@ function OpenTripList() {
         duration: '3 days 2 nights',
         order: '123',
       },
+      {
+        id: 4,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 5,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 6,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 7,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 8,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 9,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 9,
+        img: 'images/OpenTripPage/OpenTripList/open-trip1.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 9,
+        img: 'images/OpenTripPage/OpenTripList/open-trip2.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
+      {
+        id: 9,
+        img: 'images/OpenTripPage/OpenTripList/open-trip3.jpg',
+        itemName: 'Open Trip Labuan Bajo',
+        organizer: 'Nusantara Tour',
+        location: 'NTT',
+        duration: '3 days 2 nights',
+        order: '123',
+      },
     ];
 
     setData(tripData);
   }, []);
 
-  console.log(data);
+  useEffect(() => {
+    window.scrollTo({ top: 700, behavior: 'smooth' });
+  }, [currentPage]);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <section className="grid w-[1240px] grid-cols-3 gap-5">
-      {data?.map((item, index) => (
-        <OpenTripItem
-          key={index}
-          id={item.id}
-          img={item.img}
-          itemName={item.itemName}
-          organizer={item.organizer}
-          location={item.location}
-          duration={item.duration}
-          order={item.order}
-        />
-      ))}
-    </section>
+    <div>
+      <section
+        id="open-trip-list"
+        className="grid w-[1240px] grid-cols-3 gap-5"
+      >
+        {currentItems.map((item, index) => (
+          <OpenTripItem
+            key={index}
+            id={item.id}
+            img={item.img}
+            itemName={item.itemName}
+            organizer={item.organizer}
+            location={item.location}
+            duration={item.duration}
+            order={item.order}
+          />
+        ))}
+      </section>
+      <div className="flex justify-center gap-4 mt-[52px]">
+        <button
+          className="btn"
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          «
+        </button>
+
+        <button className="join-item btn">{`Halaman ${currentPage}`}</button>
+        <button
+          className="btn"
+          onClick={() => {
+            paginate(currentPage + 1);
+          }}
+          disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+        >
+          »
+        </button>
+      </div>
+    </div>
   );
 }
 
